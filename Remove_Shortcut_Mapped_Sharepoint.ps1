ceab7776426734d637fdacd12bf783524da9290b
LiamJ74 <104723680+LiamJ74@users.noreply.github.com>
2024-07-16 16:39:18 +0200

    Add files via upload

---
+++ Remove_Shortcut_Mapped_Sharepoint.ps1
@@ -0,0 +1,26 @@
+﻿function Remove-Silent {
+    param(
+        [string]$Path
+    )
+    if (Test-Path $Path) {
+        Remove-Item -Path $Path -Recurse -Force -ErrorAction SilentlyContinue
+    }
+}
+
+$baseKey = "HKCU:\Software\SyncEngines\Providers\OneDrive"
+$sousDossiers = Get-ChildItem -Path $baseKey
+
+foreach ($sousDossier in $sousDossiers) {
+    $regPath = "HKCU:\Software\SyncEngines\Providers\OneDrive\$($sousDossier.PSChildName)"
+    $isFolderScope = Get-ItemProperty -Path $regPath -Name "IsFolderScope" -ErrorAction SilentlyContinue
+
+    if ($isFolderScope -and $isFolderScope.IsFolderScope -eq 1) {
+        $mountPoint = Get-ItemProperty -Path $regPath -Name "MountPoint" -ErrorAction SilentlyContinue
+
+        if ($mountPoint -and $mountPoint.MountPoint) {
+            $folderPath = $mountPoint.MountPoint
+            Write-Host "Suppression du dossier : $folderPath"
+            Remove-Silent -Path $folderPath
+        }
+    }
+}
